9c4b6e825f3572bafdb278a6cb21c10948d0d0ee
Wajid Hussain <wajidhussain1@live.com>
2023-01-01 14:05:03 +0000

    Created my first HTML page node server
    I have created a node server that is able to print out a HTML page correctly in the right format.

--- my-first-react-project/app.js
+++ my-first-react-project/app.js
@@ -1,9 +1,18 @@
 const http = require('http')
+const fs = require('fs')
 const port = 3000
 
 const server = http.createServer(function(req, res) {
-    res.write('Hello Node')
-    res.end()
+    res.writeHead(200, { 'Content-Type': 'text/html' }) //200 means everything is okay
+    fs.readFile('index.html', function(error, data) {
+        if (error) {
+            res.writeHead(404) //404 means not found
+            res.write('Error: File was not found')
+        } else {
+            res.write(data) //just prints all data from index.html
+        }
+        res.end()
+    })
 })
 
 server.listen(port, function(error) {
