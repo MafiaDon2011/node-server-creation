3cdc50060e1b816a746f7d09004d323d39e0fd4d
Wajid Hussain <wajidhussain1@live.com>
2023-01-01 13:39:04 +0000

    Initialized a node server that is able to run
    I created a server that is able to run on port 3000 using an app.js file.
    It's fairly basic right now as it is able to just produce console text log, letting me know that the server is successfully connected and is listening.

---
+++ my-first-react-project/app.js
@@ -0,0 +1,15 @@
+const http = require('http')
+const port = 3000
+
+const server = http.createServer(function(req, res) {
+
+
+})
+
+server.listen(port, function(error) {
+    if (error) {
+        console.log('Something went wrong', error)
+    } else {
+        console.log('Our server is listening on port ' + port)
+    }
+})
