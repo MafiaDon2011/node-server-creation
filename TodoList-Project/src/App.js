87f9451e1eee81ee228c6fe529a0e80bcf3de0dc
Wajid Hussain <wajidhussain1@live.com>
2023-01-01 17:11:41 +0000

    Adding a unique ID & Making the checkboxes work
    Created a unique ID for each todo by using UUIDv4, as well as added code to make the checkboxes work when clicked by using useEffect hook.

--- TodoList-Project/src/App.js
+++ TodoList-Project/src/App.js
@@ -1,5 +1,8 @@
-import React, { useState, useRef } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import TodoList from "./TodoList";
+import { v4 as uuid } from 'uuid';
+
+const LOCAL_STORAGE_KEY = 'todoAPP.todos'
 
 function App() {
   const [todos, setTodos] = useState([])
@@ -8,14 +11,38 @@ function App() {
   function handleAddTodo(e) {
     const name = todoNameRef.current.value
     if (name === '') return
-    console.log(name)
+    setTodos(prevTodos => {
+      return [...prevTodos, { id: uuid(), name: name, complete: false}]
+    })
     todoNameRef.current.value = null
+  }
+
+  useEffect(() => {
+    const storedTodos = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY))
+    if (storedTodos) setTodos(storedTodos)
+  }, [])
+
+  function toggleTodo(id) {
+    const newTodos = [...todos]
+    const todo = newTodos.find(todo => todo.id === id)
+    todo.complete = !todo.complete
+    setTodos(newTodos)
+  }
+
+  useEffect(() => {
+    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(todos))
+  }, [todos])
+
+  const checkForEnterKey = (event) => {
+    if (event.key === 'Enter') {
+      handleAddTodo()
+    }
   }
 
   return (
     <>
-    <TodoList todos={todos}/>
-    <input ref={todoNameRef} type="text" />
+    <TodoList todos={todos} toggleTodo={toggleTodo} />
+    <input onKeyDown={checkForEnterKey} ref={todoNameRef} type="text" />
     <button onClick={handleAddTodo}>Add Todos</button>
     <button>Clear Completed</button>
     <div>0 left to do</div>
