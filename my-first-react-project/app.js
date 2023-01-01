69c9e427e05d8cee5b096192533be618c4310154
Wajid Hussain <wajidhussain1@live.com>
2023-01-01 13:48:11 +0000

    Able to render plain text from a server
    Created my first plain text render on localhost:3000 from using two lines of code. 
    res.write('Hello Node') & res.end()

--- my-first-react-project/app.js
+++ my-first-react-project/app.js
@@ -2,8 +2,8 @@ const http = require('http')
 const port = 3000
 
 const server = http.createServer(function(req, res) {
-
-
+    res.write('Hello Node')
+    res.end()
 })
 
 server.listen(port, function(error) {
